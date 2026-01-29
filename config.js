319e621c4239681810809ccc41808f919eea92c9
Dorcas-dodo <celestedorcas4@gmail.com>
2026-01-29 11:41:13 +0100

    Update OWNER_NUMBER in config.js
    Updated OWNER_NUMBER to a new value.

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || "", 
     PREFIXE: process.env.PREFIXE || ".",
     OWNER_NAME: process.env.NOM_OWNER || "Dorcas-dodo",
-    OWNER_NUMBER: process.env.NUMERO_OWNER || "24206461XXXX",
+    OWNER_NUMBER: process.env.NUMERO_OWNER || "242068079834", // Ton numéro mis à jour ✅
 
     // Paramètres de fonctionnement
     MODE: process.env.MODE || "public", // public ou self
     WELCOME: process.env.WELCOME || "true", // true ou false
-    GOODBYE: process.env.GOODBYE || "true", // true ou false (Ajouté ✅)
+    GOODBYE: process.env.GOODBYE || "true", // true ou false
     ANTILINK: process.env.ANTILINK || "true", // true ou false
     
     // Autres infos
