c582489e1ea4a9398584126efe75f7155d133f71
Dorcas-dodo <celestedorcas4@gmail.com>
2026-01-29 10:51:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,17 @@
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "", // Sera rempli sur le site d'hébergement
+    // Paramètres de base
+    SESSION_ID: process.env.SESSION_ID || "", 
     PREFIXE: process.env.PREFIXE || ".",
-    NOM_OWNER: process.env.NOM_OWNER || "Ton Nom",
-    NUMERO_OWNER: process.env.NUMERO_OWNER || "242XXXXXXXX"
+    OWNER_NAME: process.env.NOM_OWNER || "Dorcas-dodo",
+    OWNER_NUMBER: process.env.NUMERO_OWNER || "24206461XXXX",
+
+    // Paramètres de fonctionnement
+    MODE: process.env.MODE || "public", // public ou self
+    WELCOME: process.env.WELCOME || "true", // true ou false
+    GOODBYE: process.env.GOODBYE || "true", // true ou false (Ajouté ✅)
+    ANTILINK: process.env.ANTILINK || "true", // true ou false
+    
+    // Autres infos
+    BOT_NAME: "OTSUTSUKI-MD",
+    MENU_IMG: "https://raw.githubusercontent.com/Dorcas-dodo/OTSUTSUKI-MD/master/media/menu.jpg"
 };
