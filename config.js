7940635a32942780564a0ab3d6b618c5a430558e
Dorcas-dodo <celestedorcas4@gmail.com>
2026-01-30 20:15:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,19 +11,28 @@ module.exports = {
     MODE: process.env.MODE || "public", 
     
     // 🛡️ Protections & Automatisations
-    WELCOME: process.env.WELCOME === "true", // Conversion en booléen
+    WELCOME: process.env.WELCOME === "true", 
     GOODBYE: process.env.GOODBYE === "true", 
     ANTILINK: process.env.ANTILINK === "true",
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === "true", // Moderne
     
-    // 💾 Base de Données
-    DATABASE_URL: process.env.DATABASE_URL || "votre_lien_mongodb_ici",
+    // 👁️ Fonctions de visibilité (Moderne)
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === "true", // Regarde les statuts auto
+    AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES === "true", // Coche bleue auto
+    AUTO_TYPING: process.env.AUTO_TYPING === "false", // Affiche "écrit..."
     
+    // 💾 Base de Données (Mongoose)
+    DATABASE_URL: process.env.DATABASE_URL || "mongodb+srv://...", 
+    DATABASE: process.env.DATABASE || "Otsutsuki_DB",
+
     // 🎨 Identité & Médias
     BOT_NAME: process.env.BOT_NAME || "OTSUTSUKI-MD",
     MENU_IMG: process.env.MENU_IMG || "./media/menu.jpg",
     URL_RECURS: "https://files.catbox.moe/dyox3v.jpg",
+    
+    // 📢 Liens de la communauté
+    GCH: process.env.GCH || "https://whatsapp.com/channel/0029VbAoFIMA2pL9Tv1omN2K", // Groupe/Chaîne
+    DEV_WA: "242068079834", // Ton numéro direct pour le bouton support
 
-    // 🕒 Autres réglages
+    // 🕒 Réglages Régionaux
     TIMEZONE: process.env.TIMEZONE || "Africa/Brazzaville"
 };
